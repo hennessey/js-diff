4a1dd19b00abcd53798520678102692b09f08b79
John Hennessey <jghennes@gmail.com>
2017-01-13 22:48:10 -0500

    Function now produces an array with changes + handles mismatched string lengths

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("hello world");
+function stringToCharArray(str) {
+  return str.split('');
+}
+
+function jsdiff(a, b) {
+  if (typeof a !== 'string') {
+    return new Error('First argument needs to be a string');
+  }
+
+  if (typeof b !== 'string') {
+    return new Error('second argument needs to be a string');
+  }
+
+  const aChars = stringToCharArray(a);
+  const bChars = stringToCharArray(b);
+
+  let changeMap = [];
+  if (aChars.length > bChars.length) {
+    changeMap = aChars.map((aChar, i) => [aChar, bChars[i]]);
+  } else {
+    changeMap = bChars.map((bChar, i) => [aChars[i], bChar]);
+  }
+
+  return changeMap;
+}
+
+module.exports = jsdiff;
